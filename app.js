ea8dadd7da97d41ec36f350045d8cbdf3e0dec7e
gfouz <gfouz1975@gmail.com>
2022-10-12 11:39:51 -0400

    nn

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const routes = require("./routes/routes");
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
