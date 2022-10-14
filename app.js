6ea65f93f15e711fb0aea23668766486adef9cac
gfouz <gfouz1975@gmail.com>
2022-10-14 05:06:36 -0400

    restored app.js

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require("express");
+const morgan = require('morgan');
+const winston = require('winston');
+const { combine, timestamp, json } = winston.format;
 const routes = require("./routes/routes");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine( timestamp( {format: 'YYYY-MM-DD',} ), json() ),
+  transports: [
+    new winston.transports.File({
+      filename: 'requestlog.log',
+    }),
+  ],
 });
 
+const morganMiddleware = morgan(
+  ':method :url :status :res[content-length] - :response-time ms',
+  {
+    stream: {
+      // Configure Morgan to use our custom logger with the http severity
+      write: (message) => logger.info(message.trim()),
+    },
+  }
+);
+
+
+//app.use(logger('dev'));
+app.use(morganMiddleware);
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
